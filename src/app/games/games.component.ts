6e2ddcf1ae1d98566c75881267dc0372197f97f6
Jonathan Harris <harrijon@users.noreply.github.com>
2018-12-15 13:11:44 -0500

    attached my pick to games

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -3,6 +3,7 @@ import { GamesService } from './../services/games.service';
 import { Component, OnInit } from '@angular/core';
 import { ResolvedStaticSymbol } from '@angular/compiler';
 import { NotFoundError } from '../common/not-found-error';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-games',
@@ -23,31 +24,62 @@ export class GamesComponent implements OnInit {
       .subscribe(respobj => this.picks = respobj.picks);
 
     this.service.getGames(this.tid, this.wk)
-      .subscribe(respobj => this.games = respobj.games); 
+      .subscribe(respobj => {
+        this.games = respobj.games;
 
+        let gLen: number = this.games.length;
+        let pLen: number = this.picks.length;
+        let g: number = 0;
+        let p: number = 0;
+  
+        // console.log("gLen: " + gLen);
+        // console.log("pLen: " + pLen);
+
+        while (g < gLen) {
+          // console.log("g: " + g);
+          let p = 0;
+          
+          while (p < pLen) {
+            // console.log("p: " + p);
+            if (this.picks[p].gameid == this.games[g].id) {
+              this.games[g].myPick = this.picks[p].winnerID;
+              //console.log("this.games[g].myPick: " + this.games[g].myPick);
+              break;
+            }
+            p++;
+          }
+          g++;
+        }
+      }); 
+      
   }
 
 
-  picked(game, teamid: number) {
+  picked(game, teamid) {
     let retVal: Boolean = false;
 
+    // console.log("teamid: " + teamid + "  -  game.myPick: " + game.myPick);
+    if (game.myPick == teamid) {
+      retVal = true;
+      // console.log("true");
+    }
     // this.picks.forEach(function(pick) {
     //   if (pick.gameid == gameid) {
     //     retVal = true;
     //   }
     // });
-    if (this.picks !== null) {
-      let len: number = this.picks.length;
-      let i: number = 0;
+    // if (this.picks !== null) {
+    //   let len: number = this.picks.length;
+    //   let i: number = 0;
 
-      while (i < len) {
-        if ((this.picks[i].gameid == game.id) && (this.picks[i].winnerID == teamid)) {
-          retVal = true;
-          break;
-        }
-        i++;
-      }
-    }
+    //   while (i < len) {
+    //     if ((this.picks[i].gameid == game.id) && (this.picks[i].winnerID == teamid)) {
+    //       retVal = true;
+    //       break;
+    //     }
+    //     i++;
+    //   }
+    // }
 
     return retVal;
   }
