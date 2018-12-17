b0542196ff54de495f8f942380f9150bf52ce8c9
Jonathan Harris <harrijon@users.noreply.github.com>
2018-12-16 22:51:39 -0500

    still trying to figure out the picks page

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,9 +1,11 @@
+import { PickService } from './../services/pick.service';
 import { PicksService } from './../services/picks.service';
 import { GamesService } from './../services/games.service';
 import { Component, OnInit } from '@angular/core';
 import { ResolvedStaticSymbol } from '@angular/compiler';
 import { NotFoundError } from '../common/not-found-error';
 import { forEach } from '@angular/router/src/utils/collection';
+import { PickComponent } from '../pick/pick.component';
 
 @Component({
   selector: 'app-games',
@@ -13,14 +15,25 @@ import { forEach } from '@angular/router/src/utils/collection';
 export class GamesComponent implements OnInit {
   games: any[];
   picks: any[];
+  pick: PickComponent;
+  playerid: number = 95633;
   tid: number = 196;
   wk: number = 2; 
 
 
-  constructor(private service: GamesService, private picksService: PicksService) { }
+  constructor(private service: GamesService, 
+        //private pickService: PickService,
+        private picksService: PicksService) { }
 
   ngOnInit() {
-    this.picksService.getPicks(95633, this.wk)
+    // this.pickService.getEmptyObject()
+    //   .subscribe(respobj => {
+    //     this.pick = respobj.pick;
+    //     this.pick.playerid = this.playerid;
+    //   }
+    //   );
+
+    this.picksService.getPicks(this.playerid, this.wk)
       .subscribe(respobj => this.picks = respobj.picks);
 
     this.service.getGames(this.tid, this.wk)
@@ -32,22 +45,33 @@ export class GamesComponent implements OnInit {
         let g: number = 0;
         let p: number = 0;
   
+        this.pick.playerid = this.playerid;
+
         // console.log("gLen: " + gLen);
         // console.log("pLen: " + pLen);
 
         while (g < gLen) {
-          // console.log("g: " + g);
+          console.log("g: " + g);
           let p = 0;
-          
+          let newPick = this.pick;
+          this.games[g].myPick = newPick;
+
+          this.games[g].myPick.gameid = this.games[g].id;
+
+          console.log("a this.games[g].myPick.winnerID: " + this.games[g].myPick.winnerID);
+          this.games[g].myPick.winnerID = 0;
+
           while (p < pLen) {
             // console.log("p: " + p);
             if (this.picks[p].gameid == this.games[g].id) {
-              this.games[g].myPick = this.picks[p].winnerID;
-              //console.log("this.games[g].myPick: " + this.games[g].myPick);
+              this.games[g].myPick.winnerID = this.picks[p].winnerID;
+              // console.log("this.games[g].myPick.winnerID: " + this.games[g].myPick.winnerID);
               break;
             }
             p++;
           }
+
+          console.log("b this.games[g].myPick.winnerID: " + this.games[g].myPick.winnerID);
           g++;
         }
       }); 
@@ -55,13 +79,13 @@ export class GamesComponent implements OnInit {
   }
 
 
-  picked(game, teamid) {
+  picked(game, teamid: number) {
     let retVal: Boolean = false;
 
-    // console.log("teamid: " + teamid + "  -  game.myPick: " + game.myPick);
-    if (game.myPick == teamid) {
+    // console.log("game.id: " + game.id + " - teamid: " + teamid + "  -  game.myPick: " + game.myPick.winnerID);
+    if (game.myPick.winnerID == teamid) {
       retVal = true;
-      // console.log("true");
+      console.log("true");
     }
     // this.picks.forEach(function(pick) {
     //   if (pick.gameid == gameid) {
@@ -80,6 +104,35 @@ export class GamesComponent implements OnInit {
     //     i++;
     //   }
     // }
+
+    return retVal;
+  }
+
+
+  
+  updatePick(game, teamid) {
+    let retVal: Boolean = false;
+    game.myPick.winnerID = teamid;
+
+    let curPick;
+
+    if (this.picks !== null) {
+      let len: number = this.picks.length;
+      let i: number = 0;
+
+      while (i < len) {
+        if ((this.picks[i].gameid == game.id) && (this.picks[i].winnerID == teamid)) {
+          curPick = this.picks[i];
+          break;
+        }
+        i++;
+      }
+    }
+
+    curPick.winnerID = teamid;
+    
+    this.picksService.updatePick(curPick);
+    console.log("teamid: " + teamid + "  -  game.myPick: " + game.myPick);
 
     return retVal;
   }
